c0562a3d974b7cebc079db395dd16c94b2d1dba8
Ian Cowan <ian@cowanemail.com>
2021-03-06 17:39:15 -0500

    Merge sort

--- Sort/sort_list.ex
+++ Sort/sort_list.ex
@@ -1,33 +1,33 @@
 defmodule Sort do
 
-    def merge_lists(l1, l2, l1_i, l2_i) do
-        cond do
-            l1_i >= Enum.count(l1) ->
-                Enum.concat(l1, l2)
-            l2_i >= Enum.count(l2) ->
-                l1
-            Enum.at(l2, l2_i) >= Enum.at(l1, l1_i) ->
-                List.insert_at(l1, l1_i, Enum.at(l2, l2_i))
-                merge_lists(l1, l2, l1_i + 1, l2_i + 1)
-                l1
-            Enum.at(l2, l2_i) < Enum.at(l1, l1_i) ->
-                merge_lists(l1, l2, l1_i + 1, l2_i)
-                l1
-        end
-    end
-
     def merge_lists(l1, l2) do
-        merge_lists(l1, l2, 0, 0)
+        cond do
+            l1 == [] -> l2
+            l2 == [] -> l1
+            true ->
+                [l1_val | new_l1] = l1
+                [l2_val | new_l2] = l2
+
+                if l1_val < l2_val do
+                    [l1_val] ++ merge_lists(new_l1, l2)
+                else
+                    [l2_val] ++ merge_lists(l1, new_l2)
+                end
+            end
     end
 
     def merge_sort(l) do
-        if Enum.count(l) == 1 do
-            l
+        # Make sure the list is not of length 1
+        unless length(l) <= 1 do
+            # Split the lists in half
+            {l1, l2} = Enum.split(l, div(length(l), 2))
+
+            # The sorted list will be the merge of the two
+            # split lists that have been sorted as well
+            sorted = merge_lists(merge_sort(l1), merge_sort(l2))
         else
-            {l, r} = Enum.split(l, div(Enum.count(l), 2))
-            IO.inspect l
-            IO.inspect r
-            merge_lists(merge_sort(l), merge_sort(r))
+            # If the list is length 1, evaluate to the list
+            l
         end
     end
 
